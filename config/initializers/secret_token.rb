42da4ee2786998608857ca2c76531c8ac73e19c5
root <root@client23.(none)>
2012-01-20 19:16:59 +0530

    my first app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kali::Application.config.secret_token = '2f5a43d430ce9bd0c3bfd26105c768441cb54a5dedb1a6509b348f9e8aa3e92505b4f92606de1400247c13ff69507444633bd725618def6328649b453914ea52'
